10e5e397b177a46c6f014db54031b7d4c9d347c3
Kosuke Fujita <thisis.skfk03@gmail.com>
2016-11-03 01:10:44 +0900

    ｆｄｓふぁ

--- App/views.py
+++ App/views.py
@@ -129,7 +129,18 @@ ruiseki_lists.insert(0, xticks)
 
 @app.route('/')
 def hello_world():
-    form = Emuform(request.form)
+    return render_template("index.html",
+                            table470 = table470,
+                            tablesnipe =  tablesnipe,
+                            tableteam470 = tableteam470,
+                            tableteamsnipe = tableteamsnipe,
+                            tableteamsg = tableteamsg,
+                            ruiseki_lists = json.dumps(ruiseki_lists)
+                            )
+@app.route('/emurator', methods=['GET', 'POST'])
+def emu():
+    form = Emuform()
+    print(request.method,form.validate())
     if request.method == 'POST'  and form.validate():
         tohoku = teamsg_goukei_cp["東北"] + form.tohoku1.data + form.tohoku2.data
         keio = teamsg_goukei_cp["慶應義塾"] + form.keio1.data + form.keio2.data
@@ -144,13 +155,24 @@ def hello_world():
         kansai = teamsg_goukei_cp["関西"] + form.kansai1.data + form.kansai2.data
         hirosima = teamsg_goukei_cp["広島"] + form.hirosima1.data + form.hirosima2.data
         kyushu = teamsg_goukei_cp["九州"] + form.kyushu1.data + form.kyushu2.data
-        return render_template("index.html",
-                            table470 = table470,
-                            tablesnipe =  tablesnipe,
-                            tableteam470 = tableteam470,
-                            tableteamsnipe = tableteamsnipe,
-                            tableteamsg = tableteamsg,
-                            ruiseki_lists = json.dumps(ruiseki_lists),
+        flash("tohoku:{tohoku},keio:{keio},waseda:{waseda},meikai:{meiai},nihon:{nihon},tyuou:{tyuou},hosei:{hosei},dosisha:{dosisha},kyoto:{kyoto},kwansei:{kwansei},kansai:{kansai},hirosima:{hirosima},kyushu:{kyushu}"
+            .format(tohoku=tohoku,
+                            keio=keio,
+                            waseda=waseda,
+                            meikai=meikai,
+                            nihon=nihon,
+                            tyuou=tyuou,
+                            hosei=hosei,
+                            dosisha=dosisha,
+                            kyoto=kyoto,
+                            kwansei=kwansei,
+                            kansai=kansai,
+                            hirosima=hirosima,
+                            kyushu=kyushu
+                            ))
+        return redirect(url_for('register'))
+
+    return render_template("emurator.html",
                             form = form,
                             tohokunow = teamsg_goukei_cp["東北"],
                             keionow = teamsg_goukei_cp["慶應義塾"],
@@ -159,43 +181,7 @@ def hello_world():
                             nihonnow = teamsg_goukei_cp["日大"],
                             tyuounow = teamsg_goukei_cp["中央"],
                             hoseinow = teamsg_goukei_cp["法政"],
-                            disishanow = teamsg_goukei_cp["同志社"],
-                            kyotonow = teamsg_goukei_cp["京都"],
-                            kwanseinow = teamsg_goukei_cp["関西学院"],
-                            kansainow = teamsg_goukei_cp["関西"],
-                            hirosimanow = teamsg_goukei_cp["広島"],
-                            kyushunow = teamsg_goukei_cp["九州"],
-                            tohoku = tohoku,
-                            keio =keio,
-                            waseda = waseda,
-                            meikai = meikai,
-                            nihon = nihon,
-                            tyuou = tyuou,
-                            hosei = hosei,
-                            dosisha = dosisha,
-                            kyoto = kyoto,
-                            kwansei = kwansei,
-                            kansai = kansai,
-                            hirosima = hirosima,
-                            kyushu = kyushu
-                            )
-    else:
-        return render_template("index.html",
-                            table470 = table470,
-                            tablesnipe =  tablesnipe,
-                            tableteam470 = tableteam470,
-                            tableteamsnipe = tableteamsnipe,
-                            tableteamsg = tableteamsg,
-                            ruiseki_lists = json.dumps(ruiseki_lists),
-                            form = form,
-                            tohokunow = teamsg_goukei_cp["東北"],
-                            keionow = teamsg_goukei_cp["慶應義塾"],
-                            wasedanow = teamsg_goukei_cp["早稲田"],
-                            meikainow = teamsg_goukei_cp["明海"],
-                            nihonnow = teamsg_goukei_cp["日大"],
-                            tyuounow = teamsg_goukei_cp["中央"],
-                            hoseinow = teamsg_goukei_cp["法政"],
-                            disishanow = teamsg_goukei_cp["同志社"],
+                            dosishanow = teamsg_goukei_cp["同志社"],
                             kyotonow = teamsg_goukei_cp["京都"],
                             kwanseinow = teamsg_goukei_cp["関西学院"],
                             kansainow = teamsg_goukei_cp["関西"],
